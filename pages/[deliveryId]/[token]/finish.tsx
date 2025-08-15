2e95ea4bc569f89049a48cdbe6d6023cdbaeec10
jvnsantos <jvnsantos@outlook.com>
2025-08-15 17:13:54 -0300

    teste

--- pages/[deliveryId]/[token]/finish.tsx
+++ pages/[deliveryId]/[token]/finish.tsx
@@ -31,6 +31,7 @@ const FinishViwer = () => {
           overflow: 'hidden'
         }}
       >
+        
         {isLoadingViewer && (
           <div className="position-absolute top-50 start-50 translate-middle">
             <div className="d-flex flex-column align-items-center">
